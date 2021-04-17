d9972b9ea8ecccff880347a4a96778b7fe74732c
Leonardo Rocha <leoheiro@hotmail.com>
2021-04-17 15:32:40 -0300

    Algumas telas cadastro aluno

--- PI/views/alunos.php
+++ PI/views/alunos.php
@@ -10,10 +10,26 @@
         
         
         <div class = 'lista-aluno'> 
-           
-            <h3> Clique aqui para cadastrar um aluno</h3>
-            
-        
+
+
+           <div class = 'header-lista-alunos'>
+
+            <a href="index.php?view=cad_aluno"><h3> Clique aqui para cadastrar um aluno </h3> </a>
+
+            <div class = 'procura-aluno'>
+
+                <form action="">
+                    
+                    <input type="text" id="searchAluno" class="input-search-aluno" name="searchAluno" placeholder="Nome do aluno" required>
+
+                    <input type="button" class="button-search-aluno" value="Procurar"  >
+
+                </form>
+                
+
+            </div>
+
+            </div>
 
             <div class = 'aluno'>
 
@@ -21,9 +37,9 @@
 
                 <div class = 'opcoes'>
 
-                    <a href=""><span class = 'aluno-editar'> Editar </span> </a>
-                    <a href=""><span class = 'aluno-deletar'> Deletar </span> </a>
-                    <a href=""><span class = 'aluno-visu'> Visualizar </span> </a>
+                    <a href="index.php?view=edit_aluno"><span class = 'aluno-editar' onclick=""> Editar </span> </a>
+                    <a href=""><span class = 'aluno-deletar' onclick="deleteAluno('Leonardo')"> Deletar </span> </a>
+                    <a href=""><span class = 'aluno-visu' onclick="visuAluno('Leonardo')"> Visualizar </span> </a>
 
                 </div>
 
@@ -39,7 +55,21 @@
 </main>
 
 
+<script>
+    function deleteAluno(nome){
 
+        if (confirm("Você realmente que excluir "+nome+"?")){
+
+            window.alert("Excluindo "+nome);
+        }
+    }
+
+    function visuAluno(nome){
+
+        window.alert("Aluno: "+nome+"? \n Muito lindo");
+
+    }
+</script>
 
 
 </body>
---
+++ PI/views/edit_aluno.php
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+    <div class = 'main-div-cad-aluno'>
+
+
+<form>
+
+    <input type="text" id="nomeCompleto" class="form-input" name="nomeCompleto" placeholder="Nome completo do aluno" value = 'Fulano de tal' required>
+    
+    <input type="text" id="matricula" class="form-input" name="matricula" placeholder="Matricula do Aluno" value = 'Fulano de tal'required>
+    
+    <input type="button" class="button-cad" value="Concluir edições"  onclick="logIn()">
+    <br>
+    
+</form>
+</div>
+
+
+
+
+
+
+</body>
