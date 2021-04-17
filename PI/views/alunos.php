813c481d2c511044062f852dd83258318e644321
Leonardo Rocha <leoheiro@hotmail.com>
2021-04-17 14:25:35 -0300

    alterações cad_aluno

---
+++ PI/views/alunos.php
@@ -0,0 +1,45 @@
+
+
+
+
+
+<main class = 'main-alunos'>
+    
+    <div class = 'main-div-alunos'>
+        
+        
+        
+        <div class = 'lista-aluno'> 
+           
+            <h3> Clique aqui para cadastrar um aluno</h3>
+            
+        
+
+            <div class = 'aluno'>
+
+                <span class = 'nome-aluno'> Ana Clara </span>
+
+                <div class = 'opcoes'>
+
+                    <a href=""><span class = 'aluno-editar'> Editar </span> </a>
+                    <a href=""><span class = 'aluno-deletar'> Deletar </span> </a>
+                    <a href=""><span class = 'aluno-visu'> Visualizar </span> </a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+    </div>
+
+</main>
+
+
+
+
+
+</body>
