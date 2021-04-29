39db27d3a279b8d3f09e5daadab6235ba619e7c3
Leonardo Rocha <leoheiro@hotmail.com>
2021-04-29 18:52:15 -0300

    alterações hora/data edit e cad turma

---
+++ PI/js/turma_hora.js
@@ -0,0 +1,105 @@
+function horaSeg(){
+
+    input = document.getElementById('horseg');
+
+    if (input.style.display == 'block'){
+
+        input.style.display = 'none';
+        
+    } else {
+
+        input.style.display = 'block';
+
+    }
+    
+
+}
+
+    function horaTer(){
+
+    input = document.getElementById('horter');
+
+    if (input.style.display == 'block'){
+
+        input.style.display = 'none';
+        
+    } else {
+
+        input.style.display = 'block';
+
+    }
+    
+
+}
+
+function horaQua(){
+
+    input = document.getElementById('horqua');
+
+    if (input.style.display == 'block'){
+
+        input.style.display = 'none';
+        
+    } else {
+
+        input.style.display = 'block';
+
+    }
+    
+
+}  
+    function horaQui(){
+
+    input = document.getElementById('horqui');
+
+    if (input.style.display == 'block'){
+
+        input.style.display = 'none';
+        
+    } else {
+
+        input.style.display = 'block';
+
+    }
+    
+
+}
+
+function horaSex(){
+
+    input = document.getElementById('horsex');
+
+    if (input.style.display == 'block'){
+
+        input.style.display = 'none';
+        
+    } else {
+
+        input.style.display = 'block';
+
+    }
+    
+
+}
+
+function horaSab(){
+
+input = document.getElementById('horsab');
+
+if (input.style.display == 'block'){
+
+    input.style.display = 'none';
+    
+} else {
+
+    input.style.display = 'block';
+
+}
+
+
+}
+
+function addAlunos (){
+
+    window.location.replace("index.php?view=cad_turma_alunos");
+}
--- PI/views/cad_turma.php
+++ PI/views/cad_turma.php
@@ -40,32 +40,50 @@
     <input type="date" id="data" class="form-input" name="data"  required>
     
     <div class = 'dias-semana'>
-        <label ><input type="checkbox" name="aluno1" value="segunda" >
+        <label ><input type="checkbox" id = 'segunda'name="aluno1" value="segunda" onclick="horaSeg()" >
                 Segunda feira
         </label>
         <br>
-        <label ><input type="checkbox" name="aluno1" value="terca" >
+        <label ><input type="checkbox"  id = 'terca' name="aluno1" value="terca" onclick="horaTer()" >
                 Terça feira
         </label>
         <br>
-        <label ><input type="checkbox" name="aluno1" value="quarta" >
+        <label ><input type="checkbox" id = 'quarta' name="aluno1" value="quarta"onclick="horaQua()" >
                 Quarta feira
         </label>
         <br>
-        <label ><input type="checkbox" name="aluno1" value="quinta" >
+        <label ><input type="checkbox"  id ='quinta' name="aluno1" value="quinta" onclick="horaQui()">
                 Quinta feira
         </label>
         <br>
-        <label ><input type="checkbox" name="aluno1" value="sexta" >
+        <label ><input type="checkbox" id = 'sexta' name="aluno1" value="sexta" onclick="horaSex()">
                 Sexta feira
         </label>
         <br>
-        <label ><input type="checkbox" name="aluno1" value="sabado" >
+        <label ><input type="checkbox"  id = 'sabado' name="aluno1" value="sabado" onclick="horaSab()">
                 Sabado 
         </label>
         <br>
     </div>
     
+    <label for="horseg" style="display: none;" id = 'lhorseg'> Hora da segunda</label>
+    <input type="time" id="horseg" class="form-input-hora" name="horseg" placeholder="Horario da segunda" >
+
+    <label for="horter" style="display: none;" id = 'lhorter'> Hora da terça</label>
+    <input type="time" id="horter" class="form-input-hora" name="horter" placeholder="Horario da terca" >
+
+    <label for="horqua" style="display: none;" id = 'lhorqua'> Hora da quarta</label>
+    <input type="time" id="horqua" class="form-input-hora" name="horqua" placeholder="Horario da quarta" >
+
+    <label for="horqui" style="display: none;" id = 'lhorqui'> Hora da quinta</label>
+    <input type="time" id="horqui" class="form-input-hora" name="horqui" placeholder="Horario da quinta" >
+
+    <label for="horsex" style="display: none;" id = 'lhorsex'> Hora da sexta</label>
+    <input type="time" id="horsex" class="form-input-hora" name="horsex" placeholder="Horario da sexta" >
+
+    <label for="horsab" style="display: none;" id = 'lhorsab'> Hora do sabado</label>
+    <input type="time" id="horsab" class="form-input-hora" name="horsab" placeholder="Horario do sabado" >
+
     <label for="horario">Descreva o horário</label>
     <input type="text" id="horario" class="form-input" name="horario" placeholder="Descreva o horário" required>
     
@@ -76,12 +94,132 @@
 </div>
 
 
-<script>
+<script >
+
+    function horaSeg(){
+
+    input = document.getElementById('horseg');
+    label = document.getElementById('lhorseg');
+
+    if (input.style.display == 'block' && label.style.display == 'block'){
+
+        label.style.display = 'none';
+        input.style.display = 'none';
+        
+    } else {
+
+        label.style.display = 'block';
+        input.style.display = 'block';
+
+    }
+
+
+    }
+
+    function horaTer(){
+
+    input = document.getElementById('horter');
+    
+    label = document.getElementById('lhorter');
+
+    if (input.style.display == 'block' && label.style.display == 'block'){
+
+        label.style.display = 'none';
+        input.style.display = 'none';
+        
+    } else {
+
+        label.style.display = 'block';
+        input.style.display = 'block';
+
+    }
+
+    }
+
+    function horaQua(){
+
+    input = document.getElementById('horqua');
+    label = document.getElementById('lhorqua');
+
+    if (input.style.display == 'block' && label.style.display == 'block'){
+
+        label.style.display = 'none';
+        input.style.display = 'none';
+        
+    } else {
+
+        label.style.display = 'block';
+        input.style.display = 'block';
+
+    }
+
+
+    }  
+    function horaQui(){
+
+    input = document.getElementById('horqui');
+
+    label = document.getElementById('lhorqui');
+
+    if (input.style.display == 'block' && label.style.display == 'block'){
+
+        label.style.display = 'none';
+        input.style.display = 'none';
+        
+    } else {
+
+        label.style.display = 'block';
+        input.style.display = 'block';
+
+    }
+
+    }
+
+    function horaSex(){
+
+    input = document.getElementById('horsex');
+    label = document.getElementById('lhorsex');
+
+    if (input.style.display == 'block' && label.style.display == 'block'){
+
+        label.style.display = 'none';
+        input.style.display = 'none';
+        
+    } else {
+
+        label.style.display = 'block';
+        input.style.display = 'block';
+
+    }
+
+
+    }
+
+    function horaSab(){
+
+    input = document.getElementById('horsab');
+    label = document.getElementById('lhorsab');
+
+    if (input.style.display == 'block' && label.style.display == 'block'){
+
+        label.style.display = 'none';
+        input.style.display = 'none';
+        
+    } else {
+
+        label.style.display = 'block';
+        input.style.display = 'block';
+
+    }
+
+    }
 
     function addAlunos (){
 
-        window.location.replace("index.php?view=cad_turma_alunos");
+    window.location.replace("index.php?view=cad_turma_alunos");
     }
+
+
 </script>
 
 
